9f14355987fb3d9fa1e91664dd2ded55fe0aea99
Dmitri Lebedev <ryba4@ryba4.com>
2017-09-17 18:12:12 +0300

    омск-томск путались, доделал вёрстку

--- calculator/match-cities.py
+++ calculator/match-cities.py
@@ -6,14 +6,16 @@ import pandas as pd
 def main(muni_df: gpd.GeoDataFrame, pop_df: pd.DataFrame, outfile):
 	muni_df = muni_df[~muni_df['name'].str.contains('район')]
 	def _m(n):
-		l = n.lower()
+		l = n.lower().split()
 		if not l or len(l) == 0:
 			return None
 
 		for city_name in pop_df['name']:
-			if city_name.lower() in l:
+			if city_name.lower() in l:  # делим на массив, чтобы не искать во всей строке, иначе омск/томск путаются
 				return city_name
 			if city_name.endswith('ь') and city_name[:-1].lower() in l:
+				return city_name
+			if ' ' in city_name and city_name.lower() in n.lower(): # если в имени из справочника населения есть пробел (набережные челны), тогда надо сравнить всю строку
 				return city_name
 
 	muni_df['short_name'] = muni_df['name'].apply(_m)
