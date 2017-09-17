b3eecb42e6108baa32b3b10019280a35ff4d59a7
Dmitri Lebedev <ryba4@ryba4.com>
2017-09-17 20:59:01 +0300

    ошибка: длина считалась некорректно

--- calculator/stats.py
+++ calculator/stats.py
@@ -3,7 +3,7 @@ import geopandas as gpd
 
 @geolib.autoargs
 def main(df: gpd.GeoDataFrame, outfile):
-	df['lanes_per_1K'] = df.length / df.population * 1000
+	df['lanes_per_1K'] = df.lanes_length / df.population * 1000
 	df.sort_values('lanes_per_1K', ascending=False, inplace=True)
 	df = gpd.GeoDataFrame(df, crs=df.crs).to_crs(geolib.crs_dict[4326])
 	return df.join(df.bounds)
