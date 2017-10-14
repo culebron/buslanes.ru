6266e48a6de5b86b756d209db81fe56a2508ff78
Dmitri Lebedev <ryba4@ryba4.com>
2017-10-15 00:03:15 +0300

    исправление багов и api скриптов, вывел ссылку "о проекте", сделал ширину линий зависящей от числа полос в выделенке

--- calculator/dissolve.py
+++ calculator/dissolve.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-from aqtash import autoargs
+from aqtash import autoargs, utils
 from argh import CommandError
-from collections import defaultdict
-from shapely.geometry import Point
 import geopandas as gpd
-import numpy as np
-import pandas as pd
 
 
 ALLOWED_OPERATIONS = {'sum', 'avg', 'min', 'first', 'last', 'count'}
@@ -18,16 +14,14 @@ def do(in_df: gpd.GeoDataFrame, groupby, aggregations, outfile):
 	"""
 
 	operations = {}  # 'index': 'first'}  # {'geometry': 'first'}
-	for aggpair in aggregations.split(';'):
-		operation_name, field_name = aggpair.split(':')
+	for aggpair in utils.parse_column_names(aggregations):
+		operation_name, field_name = aggpair
 		if operation_name not in ALLOWED_OPERATIONS:
 			raise CommandError('unknown operation {0}, allowed operations: {1}'.format(operation_name, ', '.join(ALLOWED_OPERATIONS)))
 
 		operations[field_name] = operation_name
 
-	if groupby not in in_df:
-		raise CommandError('column "{0}" not in dataframe. Available columns: "{1}"'.format(groupby, '", "'.join(list(in_df))))
-
-	result_df = in_df.dissolve(by=groupby, aggfunc=operations)
+	groups = [g[0] for g in utils.parse_column_names(groupby)]
+	result_df = in_df.dissolve(by=groups, aggfunc=operations)
 	result_df.crs = in_df.crs
 	return result_df
--- calculator/main.py
+++ calculator/main.py
@@ -1,13 +1,12 @@
-from aqtash import autoargs, memoize, sh, WatchedFile, GOOGLE, WGS, intersect, write_file, stack, read_file
-import convert_crs
+from aqtash import autoargs, memoize, sh, WatchedFile, GOOGLE, WGS, intersect, write_file, read_file
 import dissolve
 import length
 import match_cities
+import os
 import pandas as pd
 import parsekml
 import stats
 import strip_geometry
-import os
 
 @memoize
 def group_data(map_id, borders_file):
@@ -19,24 +18,22 @@ def group_data(map_id, borders_file):
 	lengths = length.do(lanes, None, 'lanes_length').to_crs(WGS)
 
 	matched_cities = match_cities.do(borders_file, read_file('src/city-population.csv'), None)
-	merged_lanes = intersect.do(lengths, matched_cities, None)
-	return dissolve.do(merged_lanes, 'index_right', 'first:population;first:short_name;sum:lanes_length', None)
+	return intersect.do(lengths, matched_cities, None)
 
 @autoargs
 def render_page(outfile='build/index.html'):
 	print('executing main')
 	russia = group_data('1PWvRWfdKEV4SVs5ONkDgRPLbRiQ', read_file('src/muni.geojson'))
 	moscow = group_data('1GO7k2n2_8FgvzaaZCKtRhVCPr5s', read_file('src/mow.geojson'))
-	all_data = pd.concat([russia, moscow])
+	displayed_lanes = pd.concat([russia, moscow])
 
 
 	result_geojson = 'build/bus-lanes.geojson'
+	write_file(displayed_lanes, result_geojson)
+
+	stat_table = dissolve.do(displayed_lanes, 'short_name', 'sum:lanes_length;first:population', None)
+	stat_table = strip_geometry.do(stats.do(stat_table, None), None)
 	result_csv = 'build/bus-lanes.csv'
+	write_file(stat_table, result_csv)
 
-	lanes_result = stats.do(all_data, None)
-	write_file(lanes_result, result_geojson)
-
-	lanes_result = strip_geometry.do(lanes_result, None)
-	write_file(lanes_result, result_csv)
-
-	sh('python3 render.py ', WatchedFile('html/index.template.html'), ' ', result_geojson, ' ', outfile)
+	sh('python3.6 render.py ', WatchedFile('html/index.template.html'), ' ', result_geojson, ' ', result_csv, ' ', outfile)
